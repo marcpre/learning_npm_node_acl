17efd361947d42e461333105d4c3c097e71b564d
marcpre <marcus.presich@gmail.com>
2017-11-18 21:07:58 +0100

    adding hbs

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ const app = express()
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'pug')
+app.set('view engine', 'hbs')
 app.use(logger(process.env.LOG_ENV))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
